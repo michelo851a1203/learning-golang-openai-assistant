7ca284f5060e999ddcf4e5ad5b252c0dc82b2d8a
MichaelHo <michelo851a1203@gmail.com>
2023-11-20 22:01:48 +0800

    fix interface with this solution # 2023-11-20-003
    	修改：     openAiMessages/messages.interface.go

--- openAiMessages/messages.interface.go
+++ openAiMessages/messages.interface.go
@@ -4,7 +4,7 @@ import "testf/openAiType"
 
 type Messages interface {
 	CreateMessages(threadID string, createRequest *openAiType.CreateMessagesRequest) (*openAiType.OpenAiMessagesObject, error)
-	ModifyMessages(threadID string, messagesID string) (*openAiType.OpenAiMessagesObject, error)
+	ModifyMessages(threadID string, messagesID string, updateRequest *openAiType.UpdateMessagesRequest) (*openAiType.OpenAiMessagesObject, error)
 	GetMessagesList(threadID string, listRequest *openAiType.QueryListRequest) (*openAiType.ListResponse[openAiType.OpenAiMessagesObject], error)
 	GetMessages(threadID string, MessagesID string) (*openAiType.OpenAiMessagesObject, error)
 }
